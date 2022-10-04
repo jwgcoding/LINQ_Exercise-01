a7d9613a3fc571da667066b90819861672f5d3fd
Gary Li <garyli0802@yahoo.com>
2022-10-04 07:51:01 -0700

    initial commit

--- LINQ_Exercise-01/Program.cs
+++ LINQ_Exercise-01/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LINQ_Exercise_01
 {
@@ -42,21 +43,44 @@ namespace LINQ_Exercise_01
 
             // TODO: Using the dictionaryOfStrings above, remove the entries that contain numbers or an underscore
             // and print the contents to the console (using a LINQ 1-Liner)
+            dictionaryOfStrings.Where(kvp => !kvp.Value.Contains("1") &&
+                                             !kvp.Value.Contains("2") &&
+                                             !kvp.Value.Contains("3") &&
+                                             !kvp.Value.Contains("4") &&
+                                             !kvp.Value.Contains("5") &&
+                                             !kvp.Value.Contains("6") &&
+                                             !kvp.Value.Contains("7") &&
+                                             !kvp.Value.Contains("8") &&
+                                             !kvp.Value.Contains("9") &&
+                                             !kvp.Value.Contains("_"))
+                                               .ToList()
+                                            .ForEach(x => Console.WriteLine($"Key: {x.Key} \t Value: {x.Value}"));
+
 
 
 
             // TODO: Using the dictionaryOfStrings above, filter out all of the entries with vowels
             // and print the contents to the console (using a LINQ 1-Liner)
 
+            dictionaryOfStrings.Where(vowel => !vowel.Value.Contains("a") &&
+                                               !vowel.Value.Contains("e") &&
+                                               !vowel.Value.Contains("i") &&
+                                               !vowel.Value.Contains("o") &&
+                                               !vowel.Value.Contains("u"))
+                                                .ToList()
+                                                .ForEach(x => Console.WriteLine(x));
+
 
             IEnumerable<int> collectionOfNumbers = new int[] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
 
             //TODO: Using the collectionOfNumbers above, transform each number into its squared form (Hint: Select())
             //      and print the contents to the console (using a LINQ 1-Liner)
-
+            collectionOfNumbers.Select(x => Math.Pow(x, 2)).ToList().ForEach(x => Console.WriteLine(x));
 
             //TODO: Using the collectionOfNumbers above, filter for only the numbers that are evenly divisible by 3
             //      and print the contents to the console (using a LINQ 1-Liner)
+
+           collectionOfNumbers.Where(x => x % 3 == 0).ToList().ForEach(x => Console.WriteLine(x));
 
         }
     }
